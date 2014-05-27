a8a0ce70a1840aa479dcf1cc90232481b0f0bd2c
Alessandro Fragnani <alefragnani@hotmail.com>
2014-05-27 13:37:06 -0300

    Update:  - Sublime Text v3 Compatible
    New Features:
     - use_buffer setting

--- toggle_delphi_file.py
+++ toggle_delphi_file.py
@@ -5,7 +5,20 @@
 import sublime, sublime_plugin
 import os
 
+# valid_extensions = [".pas", ".dfm", ".fmx"]
+
+# .pas -> .dfm / .fmx
+# .dfm -> .pas
+# .fmx -> .pas
+# .dpr -> .dof / .cfg (circular)
+# .dpk -> .dof / .cfg (circular)
+mapping = {'.pas': '.dfm,.fmx', 
+            '.dfm': '.pas',
+            '.dpr': '.dof,.cfg'}
+
 class ToggleDelphiFileCommand(sublime_plugin.TextCommand):
+
+
     def run(self, edit):
         file_name = os.path.splitext(self.view.file_name())[0]
         file_extension = os.path.splitext(self.view.file_name())[1]
@@ -20,10 +33,25 @@ class ToggleDelphiFileCommand(sublime_plugin.TextCommand):
         else:
             sublime.status_message("Unsupported extension (" + file_extension_lowercase + ")")
             return
-
+            
         file_name_toggled = file_name + file_extension_toggle
         
         if os.path.exists(file_name_toggled):
-            self.view.window().open_file(file_name_toggled)
+            s = sublime.load_settings("toggle_delphi_file.sublime-settings")
+            use_buffer = s.get("use_buffer", "false")
+            if use_buffer:
+                self.view.window().open_file(file_name_toggled, sublime.TRANSIENT)
+            else:
+                self.view.window().open_file(file_name_toggled)
         else:
             sublime.error_message("Toggle file \"" + file_name_toggled + "\" does not exists.")
+
+    def is_visible(self):
+        file_extension = os.path.splitext(self.view.file_name())[1]
+        file_extension_lowercase = file_extension.lower()
+        #print len(valid_extensions)
+        print len(mapping)
+        print mapping[".pas"]
+        print mapping.keys()
+        print "splitting" + str(mapping[".pas"].split(",")[0])
+        return file_extension_lowercase == ".pas"
